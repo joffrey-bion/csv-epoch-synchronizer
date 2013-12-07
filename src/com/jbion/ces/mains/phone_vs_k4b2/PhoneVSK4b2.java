99720fe60b6aa4ee8d4dcb3e0bf582957885e1a2
Joffrey BION <joffrey.bion@gmail.com>
2013-12-07 18:39:13 +0100

    modified exception handling to display full stack trace on I/O exceptions. corrected deleted temp file second message.

--- src/com/jbion/ces/mains/phone_vs_k4b2/PhoneVSK4b2.java
+++ src/com/jbion/ces/mains/phone_vs_k4b2/PhoneVSK4b2.java
@@ -23,13 +23,13 @@ import com.jbion.utils.fpgui.gwindows.JFileProcessorWindow;
 import com.jbion.utils.xml.serialization.parameters.SpecificationNotMetException;
 
 public class PhoneVSK4b2 {
-    
+
     static final int INPUT_PHONE = 0;
     static final int INPUT_K4B2 = 1;
     static final int INPUT_PARTICIPANT = 2;
     static final int INPUT_XML_TREE = 3;
     static final int OUTPUT_VALIDATION = 0;
-    
+
     public static void main(String[] args) {
         if (args.length == 0) {
             SwingUtilities.invokeLater(new Runnable() {
@@ -51,7 +51,8 @@ public class PhoneVSK4b2 {
         LookAndFeel.setSystemLookAndFeel();
         // file pickers source and destination
         final JFilePickersPanel filePickers = new JFilePickersPanel(new String[] {
-                "Phone raw file", "K4b2 test file", "Participant file", "XML Tree file" }, "Validation output file");
+                "Phone raw file", "K4b2 test file", "Participant file", "XML Tree file" },
+                "Validation output file");
         FilePicker[] ifps = filePickers.getInputFilePickers();
         ifps[INPUT_PHONE].addFileTypeFilter(".csv", "Comma-Separated Values file");
         ifps[INPUT_K4B2].addFileTypeFilter(".csv", "Comma-Separated Values file");
@@ -95,6 +96,7 @@ public class PhoneVSK4b2 {
                 }
             } catch (IOException e) {
                 System.err.println("I/O error: " + e.getMessage());
+                e.printStackTrace();
             } catch (SAXException e) {
                 System.err.println("Incorrect XML file: " + e.getMessage());
             } catch (SpecificationNotMetException e) {
@@ -104,48 +106,43 @@ public class PhoneVSK4b2 {
         }
     }
 
-    public static HashMap<Phase, Accuracy> analyze(PvKParams params) {
-        try {
-            K4b2StatsCalculator k4 = new K4b2StatsCalculator(params.k4b2File);
-            Results res = k4.getAllStats(params.nbSyncMarkers);
-            PhasePhoneParams ppp = new PhasePhoneParams(params);
-            HashMap<Phase, Accuracy> accuracies = new HashMap<>();
-            for (Phase p : Phase.values()) {
-                PhaseResults pr = res.get(p);
-                ppp.setPhaseResults(p, pr);
-                RawToEpConverter.createEpochsFile(ppp);
-                HashMap<String, Integer> lvlsDistrib = LabelAppender.appendLabels(ppp);
-                if (Config.get().deleteTempFiles) {
-                    if (new File(ppp.getUnlabeledDatasetFilePath()).delete()) {
-                        System.out.println("Temp file deleted (" + ppp.getUnlabeledDatasetFilePath() + ")");
-                    }
-                    if (new File(ppp.getLabeledDatasetFilePath()).delete()) {
-                        System.out.println("Temp file deleted (" + ppp.getUnlabeledDatasetFilePath() + ")");
-                    }
+    private static HashMap<Phase, Accuracy> analyze(PvKParams params) throws IOException,
+            SAXException, SpecificationNotMetException {
+        K4b2StatsCalculator k4 = new K4b2StatsCalculator(params.k4b2File);
+        Results res = k4.getAllStats(params.nbSyncMarkers);
+        PhasePhoneParams ppp = new PhasePhoneParams(params);
+        HashMap<Phase, Accuracy> accuracies = new HashMap<>();
+        for (Phase p : Phase.values()) {
+            PhaseResults pr = res.get(p);
+            ppp.setPhaseResults(p, pr);
+            RawToEpConverter.createEpochsFile(ppp);
+            HashMap<String, Integer> lvlsDistrib = LabelAppender.appendLabels(ppp);
+            if (Config.get().deleteTempFiles) {
+                if (new File(ppp.getUnlabeledDatasetFilePath()).delete()) {
+                    System.out.println("Temp file deleted (" + ppp.getUnlabeledDatasetFilePath()
+                            + ")");
                 }
-                HashMap<String, Double> lvlsTimeDistrib = new HashMap<>();
-                for (String level : lvlsDistrib.keySet()) {
-                    lvlsTimeDistrib.put(level,
-                            (double) (lvlsDistrib.get(level) * Config.get().epochWidthVsK4b2));
+                if (new File(ppp.getLabeledDatasetFilePath()).delete()) {
+                    System.out.println("Temp file deleted (" + ppp.getLabeledDatasetFilePath()
+                            + ")");
                 }
-                System.out.println("K4b2 levels distribution:");
-                System.out.println(pr.getLevelsDistribution());
-                System.out.println("Phone levels distribution:");
-                System.out.println(lvlsTimeDistrib);
-                accuracies.put(p, new Accuracy(lvlsTimeDistrib, pr.getLevelsDistribution()));
             }
-            System.out.println();
-            System.out.println("Writing results...");
-            new PrintableAccuracies(accuracies).writeResults(params.participantFile, params.outputValidationFile);
-            System.out.println("Results written in " + params.outputValidationFile);
-            return accuracies;
-        } catch (IOException e) {
-            System.err.println("I/O error: " + e.getMessage());
-        } catch (SAXException e) {
-            System.err.println("Incorrect XML file: " + e.getMessage());
-        } catch (SpecificationNotMetException e) {
-            System.err.println("Incorrect participant file: " + e.getMessage());
+            HashMap<String, Double> lvlsTimeDistrib = new HashMap<>();
+            for (String level : lvlsDistrib.keySet()) {
+                lvlsTimeDistrib.put(level,
+                        (double) (lvlsDistrib.get(level) * Config.get().epochWidthVsK4b2));
+            }
+            System.out.println("K4b2 levels distribution:");
+            System.out.println(pr.getLevelsDistribution());
+            System.out.println("Phone levels distribution:");
+            System.out.println(lvlsTimeDistrib);
+            accuracies.put(p, new Accuracy(lvlsTimeDistrib, pr.getLevelsDistribution()));
         }
-        return null;
+        System.out.println();
+        System.out.println("Writing results...");
+        new PrintableAccuracies(accuracies).writeResults(params.participantFile,
+                params.outputValidationFile);
+        System.out.println("Results written in " + params.outputValidationFile);
+        return accuracies;
     }
 }
